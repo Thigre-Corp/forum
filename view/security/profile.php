35746b9e68d6617b915f09546445158c49aa5aa7
Thierry GRELL <thigre.sspi@gmail.com>
2025-07-25 15:32:01 +0200

    debut CSS

--- view/security/profile.php
+++ view/security/profile.php
@@ -7,54 +7,41 @@ $user = $result["data"]['user'];
     //$user['role']= (json_decode($user['role'])[0]);
 ?>
 
-<h1>Profil Utilisateur de <?=$user//['nickName']
-?></h1>
+<h1>Profil Utilisateur de <?=$user?></h1>
 
 <div class="profile box">
     <form action="index.php?ctrl=security&action=modUser&id=<?=$user->getId()?>" method="POST">
+        <div class="Box pseudo">
+            <label for="nickName">Pseudo :</label><br>
+            <input style="width :90%;" type="text" name="nickName" id="nickName" value="<?=$user->getNickName()?>"></input>
+        </div>
+        <div class="Box email">
+            <label for="email">Email :</label><br>
+            <input style="width :90%;" type="text" name="email" id="email" value="<?=$user->getEmail()?>"></input>
+        </div>
+
 <?php
-
-foreach($user as $userKey => $userValue ){
-
-    if($userKey == 'password' || $userKey == 'id' || $userKey =='creationDate' || $userKey =='role'){
-        continue;
-    }
-
-    
-?>
-    <div class="Box <?=$userKey?>">
-        <label for="<?=$userKey?>"><?=$userKey?></label><br>
-        <input style="width :90%;" type="text" name="<?=$userKey?>" id="<?=$userKey?>" value="<?=$userValue?>"></input>
-    </div>
-    <div class="Box <?=$userKey?>">
-        <label for="<?=$userKey?>"><?=$userKey?></label><br>
-        <input style="width :90%;" type="text" name="<?=$userKey?>" id="<?=$userKey?>" value="<?=$userValue?>"></input>
-    </div>
-
-<?php } 
-
 if (App\Session::isAdmin()){ 
-
 ?>
 
-    <div class="Box role">
-        <label for="role">Changer le rôle:</label><br>
-        <select style="width :90%;" type="text" name="role" id="role">
+        <div class="Box role">
+            <label for="role">Changer le rôle:</label><br>
+                <select style="width :90%;" type="text" name="role" id="role">
 <?php
             foreach(User::USER_ROLE as $role){
 
                 echo "<option value='".$role."'>".$role."</option>";
             }
 ?>
-        </select>
-    </div>
+            </select>
+        </div>
 
 <?php
 }
 
 ?>
         <label for="suppr">Supprimer le profil ?</label>
-        <input type='checkbox' name="suppr" id="suppr"></input><br><br>
+            <input type='checkbox' name="suppr" id="suppr"></input><br><br>
         <input type='submit' value='Appliquer les modifications'></input>
         <input type='hidden' name='cestSReffe' value='<?=$_SESSION["cestSReffe"]?>'>
     </form>
